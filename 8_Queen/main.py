e5959641519979f4daed7b4fc4caf91813c32c03
ReOneK <959512421@qq.com>
2018-11-28 16:28:28 +0800

    update

--- 8_Queen/main.py
+++ 8_Queen/main.py
@@ -2,19 +2,17 @@ import random
 def conflict(state, nextX):
     nextY = len(state)
     for i in range(nextY):
-        if abs(state[i]-nextX) in (0, nextY-i):
-        #这里用两个点之差算横坐标，不能等于纵坐标的差。来确保对角线问题。
+        if abs(state[i]-nextX) in (0, nextY-i):   #确保相同行和对角线没有皇后
             return True
     return False
 
 def queens(num, state=()): 
 
-    for pos in range(num):
-        if not conflict(state, pos):#这个if是没有else的，因为当跑到conflict那里为真的时候，层层stack就被返上来，取消掉了整个这组排列（也就是没结果）
+    for pos in range(num):          #遍历所有列，然后在确保每一列的每一行和对角线无皇后
+        if not conflict(state, pos):
             if len(state) == num-1:
                 yield (pos, )
             else:
-            #这里多种组合分叉着走，也是遍历的关键。
                 for result in queens(num, state+(pos,)):
                     yield (pos, ) + result
 
